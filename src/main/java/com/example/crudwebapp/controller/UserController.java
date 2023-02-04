d81184eedd3caaf48ea8f5b2ffcaed75feff2e74
krupk <MaxKR1994>
2023-02-04 22:30:33 +0200

    created new GET endpoint (getById)

--- src/main/java/com/example/crudwebapp/controller/UserController.java
+++ src/main/java/com/example/crudwebapp/controller/UserController.java
@@ -1,12 +1,10 @@
 package com.example.crudwebapp.controller;
 
+import com.example.crudwebapp.exception.UserNotFoundException;
 import com.example.crudwebapp.model.User;
 import com.example.crudwebapp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +21,11 @@ public class UserController {
     @GetMapping("/users")
     List<User> getAllUsers(){
         return userRepository.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(id));
     }
 }
