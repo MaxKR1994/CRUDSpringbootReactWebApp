3c184f806eb8f4a63776919ab4a32501fc6de73f
krupk <MaxKR1994>
2023-02-04 22:08:00 +0200

    refactored imports frameworks

--- src/main/java/com/example/crudwebapp/controller/UserController.java
+++ src/main/java/com/example/crudwebapp/controller/UserController.java
@@ -3,14 +3,12 @@ package com.example.crudwebapp.controller;
 import com.example.crudwebapp.model.User;
 import com.example.crudwebapp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
 
     @Autowired
@@ -24,4 +22,6 @@ public class UserController {
     List<User> getAllUsers(){
         return userRepository.findAll();
     }
+
+
 }
